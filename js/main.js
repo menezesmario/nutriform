7f86074a4255be729a16d837b7e2e3d58bf79b0b
Mario <mario082@gmail.com>
2020-06-17 08:13:44 -0300

    insert date(form) in table

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ for (let i = 0; i < pacientes.length; i++) {
   }
 
   if (altura <= 0 || altura >= 3) {
-    console.log("altura inválida");
     alturaValida = false;
     tdImc.textContent = "Altura Inválida";
     paciente.classList.add("campo-invalido");
@@ -40,19 +39,49 @@ for (let i = 0; i < pacientes.length; i++) {
 let botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); //evita procedimento padrão do clique ao botão
+
+  //Capturar os dados do formulário HTML para o JS
+
+  //seleciona o formulário para captar os dados
   let form = document.querySelector("#form-adiciona");
 
+  //captura o valor digitado
   let nome = form.nome.value; //recebe o valor do campo
   let peso = form.peso.value;
   let altura = form.altura.value;
   let gordura = form.gordura.value;
+  //Dados capturados
 
+  //criar elementos no HTML com JS
+  //linha
   let pacienteTr = document.createElement("tr");
 
+  //colunas
   let nomeTd = document.createElement("td");
   let pesoTd = document.createElement("td");
   let alturaTd = document.createElement("td");
-  let gordutaTd = document.createElement("td");
+  let gorduraTd = document.createElement("td");
   let imcTd = document.createElement("td");
+
+  //insere os dados às TDs
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  //insere dentro da linha "pacienteTr" os dados das colunas "?Td"
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  //seleciona #tabela-pacientes
+  let tabela = document.querySelector("#tabela-pacientes");
+
+  //insere a linha pacienteTr à tabela
+  tabela.appendChild(pacienteTr);
+
+  console.log(pacienteTr);
+  console.log(nomeTd);
 });
